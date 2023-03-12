f4735056b606f35cf783d4f391bbf1611adf2bc1
jacklusy <jackalloussi23@gmail.com>
2023-03-12 15:36:00 +0300

    LaslesVPN

---
+++ task1/task1.js
@@ -0,0 +1,10 @@
+
+function returnBG() {
+    document.getElementById('button').style.backgroundColor = "#F53838";
+    document.getElementById('button').style.color = "#FFECEC";
+
+}
+function changeBG() {
+    document.getElementById('button').style.backgroundColor = "#FFECEC";
+    document.getElementById('button').style.color = "#F53838";
+}
